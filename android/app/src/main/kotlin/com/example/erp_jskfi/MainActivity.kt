44f0650600074852028de14e7a57f1bb4e89490f
Sri J V <srinipandiyan17300@gmail.com>
2023-03-14 12:24:52 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/erp_jskfi/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.erp_jskfi
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'JSKFI',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomepage(),
+    );
+  }
+}
+
+
+class MyHomepage extends StatefulWidget {
+  const MyHomepage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomepage> createState() => _MyHomepageState();
+}
+
+class _MyHomepageState extends State<MyHomepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
